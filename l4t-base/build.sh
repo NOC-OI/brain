f4d6609f724aef21dd081c40c79d97b3ee6790e1
Alex Baldwin <alewin@noc.ac.uk>
2025-08-08 08:56:06 +0100

    Updating gitignore and moving l4t-base to its own repo

--- l4t-base/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-./download-libs.sh
-docker buildx build -t brain/l4t-base:j62-r36.4-0 ./
