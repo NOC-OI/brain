b4dc9fba711818ba302f6907ea65f5ac5d3b1038
Alex Baldwin <alewin@noc.ac.uk>
2025-08-18 14:48:29 +0100

    Bump l4t container version

--- build-all.sh
+++ build-all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
-if [ -z "$(docker images -q docker-repo.bodc.me/oceaninfo/l4t-base:j62-r36.4-1 2> /dev/null)" ]; then
-    if [ -d "l4t-base-j62-r36.4-1" ]; then
-        ./l4t-base-j62-r36.4-1/build.sh
+if [ -z "$(docker images -q docker-repo.bodc.me/oceaninfo/l4t-base:j62-r36.4-2 2> /dev/null)" ]; then
+    if [ -d "l4t-base-j62-r36.4-2" ]; then
+        ./l4t-base-j62-r36.4-2/build.sh
     else
-        wget https://github.com/NOC-OI/l4t-base/archive/refs/tags/j62-r36.4-1.zip
-        unzip j62-r36.4-1.zip
-        rm j62-r36.4-1.zip
-        ./l4t-base-j62-r36.4-1/build.sh
+        wget https://github.com/NOC-OI/l4t-base/archive/refs/tags/j62-r36.4-2.zip
+        unzip j62-r36.4-2.zip
+        rm j62-r36.4-2.zip
+        ./l4t-base-j62-r36.4-2/build.sh
     fi
 else
-    echo "docker-repo.bodc.me/oceaninfo/l4t-base:j62-r36.4-1 available locally, skipping build of base container."
+    echo "docker-repo.bodc.me/oceaninfo/l4t-base:j62-r36.4-2 available locally, skipping build of base container."
 fi
 ./vision/build.sh
 ./asea2-camera-if/build.sh
